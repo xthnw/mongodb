52ed357f5eb9740679defadfdef157bdb071a9f3
Thanawan Sutthasena <thanawan477@gmail.com>
2024-03-20 17:46:34 +0700

    fetched data from db to frontend

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
+import TopicsList from "@/components/TopicsList";
 
 export default function Home() {
-  return (
-    <h1>Hello</h1>
-  );
+  return <TopicsList />;
 }
---
+++ components/TopicsList.jsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import RemoveButton from "./RemoveButton";
+import { HiPencilAlt } from "react-icons/hi";
+
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
+
+export default async function TopicsList() {
+  const { topics } = await getTopics();
+
+  return (
+    <>
+      {topics.map((topic) => (
+        <div
+          key={topic.id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{topic.title}</h2>
+            <div>{topic.description}</div>
+          </div>
+          <div className="flex gap-2">
+            <RemoveButton />
+            <Link href={`editTopic/${topic._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
